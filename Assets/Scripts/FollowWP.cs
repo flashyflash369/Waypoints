255f5eda8553b10a73834a7db6368d5b6e4d864a
black_hole <sdarksoul99@gmail.com>
2023-09-07 21:30:44 +0200

    tracker-commit
    Better tracking of the tranks by implemeting a tracker following perfectly the track and the tanks following their repective tracker.

--- Assets/Scripts/FollowWP.cs
+++ Assets/Scripts/FollowWP.cs
@@ -11,18 +11,52 @@ public class FollowWP : MonoBehaviour
     public float speed;
     public float rotSpeed;
     public float distance_from_next_waypoint;
+    public float distance_from_tracker;
+
+    private GameObject tracker;
+    public float tracker_speed_factor = 1;
+
+    private void InitTracker()
+    {
+        tracker = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        
+        tracker.transform.position = this.transform.position;
+        tracker.transform.rotation = this.transform.rotation;
+
+    }
+
+    private void Tracker()
+    {
+
+        if(Vector3.Distance(tracker.transform.position, this.transform.position) > distance_from_tracker)
+        {
+            return;
+        }
+
+        if(Vector3.Distance(tracker.transform.position, waypoints[m_currentWaypoint].transform.position) < distance_from_next_waypoint)
+        {
+            m_currentWaypoint = (m_currentWaypoint + 1) % waypoints.Length;
+        }   
+
+        Destroy(tracker.GetComponent<Collider>());
+        tracker.GetComponent<MeshRenderer>().enabled = false;
+
+        tracker.transform.LookAt(waypoints[m_currentWaypoint].transform);
+        tracker.transform.Translate(0, 0, (speed + tracker_speed_factor) * Time.deltaTime); 
+    }
+
+    private void Start()
+    {
+        InitTracker();
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if(Vector3.Distance(this.transform.position, waypoints[m_currentWaypoint].transform.position) < distance_from_next_waypoint)
-        {
-            m_currentWaypoint = (m_currentWaypoint + 1) % waypoints.Length;
-        }
+        Tracker();
 
-        //this.transform.LookAt(waypoints[m_currentWaypoint].transform);
 
-        Quaternion rotation = Quaternion.LookRotation(waypoints[m_currentWaypoint].transform.position - this.transform.position);
+        Quaternion rotation = Quaternion.LookRotation(tracker.transform.position - this.transform.position);
         this.transform.rotation = Quaternion.Slerp(this.transform.rotation, rotation, rotSpeed * Time.deltaTime);
         this.transform.Translate(0, 0, speed * Time.deltaTime);
     }
